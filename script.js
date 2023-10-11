b129a98f7c341f2300519272109772ea68fd3844
Gi <giovanna.kethleen@yahoo.com.br>
2023-10-11 20:01:04 -0300

    my first project

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function toggleMode() {
   html.classList.toggle("light")
 
   // pegar a tag img
-  const img = document.querySelector("#profile img")
+  // const img = document.querySelector("#profile img")
 
   // substituir a imagem
-  if (html.classList.contains("light")) {
-    // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/perfil.dia.png")
-  } else {
-    // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("scr", "./assets/perfil.gio.png")
-  }
+  // if (html.classList.contains("light")) {
+  // se tiver light mode, adicionar a imagem light
+  // img.setAttribute("src", "./assets/sun.png")
+  // } else {
+  // set tiver sem light mode, manter a imagem normal
+  // img.setAttribute("scr", "./assets/moon.gio.png")
+  // }
 }
